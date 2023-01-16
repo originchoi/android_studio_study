9b8f38f6a83b3092cec49317794b1e6b4666c88f
Origin Choi <kyun4525@gmail.com>
2023-01-16 11:34:18 +0900

    step06customadpater 모듈 추가함

--- step06customadapter/src/main/java/com/example/step06customadapter/CountryDto.java
+++ step06customadapter/src/main/java/com/example/step06customadapter/CountryDto.java
@@ -1,6 +1,8 @@
 package com.example.step06customadapter;
 
-public class CountryDto {
+import java.io.Serializable;
+
+public class CountryDto implements Serializable {
     //필드
     private int resId; //출력할 이미지 리소스 아이디 R.id.austria 등등의 값
     private String name; //나라의 이름
