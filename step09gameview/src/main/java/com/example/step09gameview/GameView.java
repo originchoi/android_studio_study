65a1871a22df74f73fa41cbd158494b53bd87dcc
Origin Choi <kyun4525@gmail.com>
2023-01-25 09:31:55 +0900

    step09gameview 에 효과음 구현함

--- step09gameview/src/main/java/com/example/step09gameview/GameView.java
+++ step09gameview/src/main/java/com/example/step09gameview/GameView.java
@@ -57,6 +57,8 @@ public class GameView extends View {
     int postCount;
     //점수
     int point;
+    //효과음을 재생해주는 객체
+    SoundManager soundManager;
 
     public GameView(Context context) {
         super(context);
@@ -64,6 +66,11 @@ public class GameView extends View {
 
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    //SoundManager 객체를 전달 받아서 필드에 저장하는 메소드
+    public void setSoundManager(SoundManager soundManager) {
+        this.soundManager = soundManager;
     }
 
     //초기화 메소드
@@ -265,6 +272,9 @@ public class GameView extends View {
                         m.y < e.y + unitSize/2;
 
                 if(isStrike && !e.isFall){//현재 추락중인 적기는 무시 하기
+                    //효과음 재생
+                    soundManager.playSound(GameActivity.SOUND_SHOOT);
+
                     //적기 에너지를 줄이고
                     e.energy -= 50;
                     //미사일을 없앤다
@@ -351,6 +361,8 @@ public class GameView extends View {
     public void missileService(){
         //미사일 만들기
         if(count%5 == 0){
+            //미사일 발사음 재생
+            soundManager.playSound(GameActivity.SOUND_LAZER);
             missList.add(new Missile(dragonX, dragonY));
         }
         //미사일 움직이기
